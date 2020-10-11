2343f49aa671beac1d88311e0f9285651ed4525d
finnjamin <finndarragh@gmail.com>
2020-10-11 17:19:21 +0100

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,55 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "index.js",
+    library: "react-jodit-editor",
+    libraryTarget: "commonjs2",
+  },
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: { loader: "babel-loader" },
       },
       {
         test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
+        use: [{ loader: "html-loader" }],
       },
     ],
   },
+  resolve: {
+    alias: {
+      react: `${__dirname}/./node_modules/react`,
+      "react-dom": `${__dirname}/./node_modules/react-dom"`,
+    },
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
+      template: "./index.html",
+      filename: "index.html",
     }),
   ],
 };
